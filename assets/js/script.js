48afe29c0d04370917fcad22102d73377213b2f0
emeliehansson <emeliehansson_@hotmail.com>
2022-10-29 14:45:45 +0000

    Add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,11 +29,14 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
-    } else {
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
+    }
+    else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
-    
+
 }
 /**
  * Checks the answer against the first element in
@@ -67,10 +70,13 @@ function calculateCorrectAnswer() {
 
     if (operator === "+"){
         return [operand1 + operand2, "addition"];
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
     } else {
         alert (`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
+
 }
 
 /**
@@ -95,6 +101,7 @@ function incrementScore() {
 
 
 function displayAdditionQuestion(operand1, operand2) {
+    
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "+";
@@ -104,6 +111,9 @@ function displaySubtractQuestion () {
 
 }
 
-function displayMultiplyQuestion () {
+function displayMultiplyQuestion (operand1, operand2) {
 
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 }
